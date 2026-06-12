39732dd694abb14b28dcd84b174c4ae802d0eb0e
Pedro Henrique Trajano da Silva <pedroht1p7@gmail.com>
2026-06-11 23:35:35 -0300

    config: fix duplicate spring key in application.yml, add multipart and static resources config

--- src/main/java/com/studify/config/SecurityConfig.java
+++ src/main/java/com/studify/config/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
             "/v3/api-docs.yaml",
             "/swagger-ui/**",
             "/swagger-ui.html",
+            "/uploads/**",
             "/actuator/**"
     };
 
